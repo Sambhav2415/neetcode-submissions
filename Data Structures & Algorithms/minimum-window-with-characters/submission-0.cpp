c4b0ed6f9ce4b9275e167f274dc8dfcebf8c2243
Sambhav2415 <sambhav2415@gmail.com>
2026-06-20 16:57:13 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> freq1,freq2;
+
+    bool toCheck(){
+        for(int i=0;i<128;i++){
+            if(freq2[i] > freq1[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+        int a = s.size();
+        int b = t.size();
+        if(b>a){
+            return "";
+        }
+
+        freq1.assign(128,0);
+        freq2.assign(128,0);
+
+        string ans = "";
+        for(char c:t){
+            freq2[c]++;
+        }
+
+        int l = 0;
+        int bestLen = INT_MAX;
+        int bestStart = -1;
+
+        for(int r=0;r<a;r++){
+            freq1[s[r]]++;
+            
+            while(toCheck()){
+                int currLen = r-l+1;
+                if(currLen < bestLen){
+                    bestLen = currLen;
+                    bestStart = l;
+                }
+                freq1[s[l++]]--;
+            }
+        }
+        if(bestStart == -1){
+            return "";
+        }
+        
+        return s.substr(bestStart,bestLen);
+        
+    }
+};
