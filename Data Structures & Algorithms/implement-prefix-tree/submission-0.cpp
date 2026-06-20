153af9f9288ec40cfdb57a97f002778649bbb824
Sambhav2415 <sambhav2415@gmail.com>
2026-06-20 12:28:41 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,49 @@
+class Node{
+public:
+    unordered_map<char , Node*> children;
+    bool endOfWord;
+    Node(){
+        endOfWord = false;
+    }
+};
+
+class PrefixTree {
+    Node* root;
+public:
+    PrefixTree() {
+        root = new Node();
+    }
+    
+    void insert(string key) {
+        Node* temp = root;
+        for(int i = 0 ; i < key.size() ; i++){
+            if(!temp->children.count(key[i])){ // key not exist before
+                temp->children[key[i]] = new Node();
+            }
+            temp = temp->children[key[i]];
+        } 
+        temp->endOfWord = true;
+    }
+    
+    bool search(string key) {
+        Node* temp = root;
+        for(int i = 0 ; i < key.size() ; i++){
+            if(!temp->children.count(key[i])){ // key not exist before
+                return false;
+            }
+            temp = temp->children[key[i]];
+        } 
+        return temp->endOfWord;
+    }
+    
+    bool startsWith(string key) {
+        Node* temp = root;
+        for(int i = 0 ; i < key.size() ; i++){
+            if(!temp->children.count(key[i])){ // key not exist before
+                return false;
+            }
+            temp = temp->children[key[i]];
+        } 
+        return true;
+    }
+};
